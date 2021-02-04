8e8efd4e5b9389c29e916884c4e632889c2faec1
Kevin Schmidt <kevinschmidt_113@hotmail.com>
2021-02-04 16:15:19 +0100

    Update dump-shellcode.sh

--- dump-shellcode.sh
+++ dump-shellcode.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "[+] Enter binary file name to create shellcode {format:\x00} [+]"
-read filename
+if [ $# -eq 0 ]
+then
+	echo "[+] FORMAT ERROR: should be :  $0 <filename>"
+	exit 1
+fi
+
 echo "[+] Shellcode: "
 echo ""
-for i in `objdump -d $filename -M intel | tr "\t" " " | tr " " "\n" | egrep "^[0-9a-f]{2}$"`; do echo -n "\x$i"; done
+for i in `objdump -d "$1" -M intel | tr "\t" " " | tr " " "\n" | egrep "^[0-9a-f]{2}$"`; do echo -n "\x$i"; done
+echo "" #Empty echo's for formating
